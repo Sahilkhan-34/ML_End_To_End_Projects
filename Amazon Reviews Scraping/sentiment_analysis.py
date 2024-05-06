9dedbc5d0db7118e91c87f2fc5a7ac1a697a15c2
sahil khan <sahilkhan.edu@gmail.com>
2024-05-06 18:53:24 +0530

    amazon reviews sentiment analysis

---
+++ Amazon Reviews Scraping/sentiment_analysis.py
@@ -0,0 +1,37 @@
+# Importing necessary modules
+import streamlit as st 
+import google.generativeai as genai
+import google.ai.generativelanguage as glm
+import pandas as pd
+
+genai.configure(api_key="AIzaSyALIvB-IxfKhe0Zxzdii2TEqTmefMxPk5M")
+
+if __name__=='__main__':
+
+    st.header("Sentiment Analysis of Reviews")
+    data = st.sidebar.file_uploader("Upload Data File Here", type=['csv'])
+
+    if data is not None:
+        data_file  = pd.read_csv(data)
+
+        Sentiment = []
+        for reviews in data_file['review_text']:
+             
+             # model selection
+             model = genai.GenerativeModel('gemini-pro')
+
+             prompt = """ Give the Sentiment analysis of given review only in two words either **POSITIVE**👍🏻👍🏻  or **NEGATIVE** 👎🏻👎🏻 """
+
+             response = model.generate_content([prompt, reviews])
+             st.write(reviews)
+             st.write(response.text)
+             Sentiment.append(response.text)
+             st.write("===============================================================================")
+
+        data_file['Sentiment'] = Sentiment
+
+        st.write(data_file)
+
+
+
+    
